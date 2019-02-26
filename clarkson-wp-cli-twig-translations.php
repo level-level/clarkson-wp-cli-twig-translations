6bb5d3a267fe5091a9972c60ade19af65e406e23
Niels de Blaauw <niels@level-level.com>
2019-02-26 16:04:39 +0100

    Allows twig translation to be generated without loading WP

--- clarkson-wp-cli-twig-translations.php
+++ clarkson-wp-cli-twig-translations.php
@@ -1,10 +1,7 @@
 <?php
 
 /*
-Plugin Name: Clarkson WP-CLI Twig Translation
-Plugin URI: https://github.com/level-level/clarkson-wp-cli-twig-translations
 Description: Smart WP-CLI command to cache the Twig templates so they can be read by PoEdit
-Version: 0.1.0
 Author: Level Level
 Author URI: https://level-level.com
 License: GPL 2.0
@@ -14,44 +11,68 @@ namespace Clarkson\WPCLITwigTranslations;
 if( !defined('WP_CLI') || ! WP_CLI ) {
     return;
 }
-class Gettext extends \WP_CLI_Command {
 
-    /**
-     * @alias prepare-files
-     */
-    public function prepareFiles(){
-        $twig_args = array(
-            'debug' => true,
-            'cache' => apply_filters( 'clarkson_twig_translations_cache_path', realpath( get_stylesheet_directory() ) . '/dist/rendered-templates/' ),
-            'auto_reload' => true
-        );
-
-        $basedir = apply_filters( 'clarkson_twig_translations_templates_path', realpath( get_stylesheet_directory() . '/templates/' ) );
-        $twig_fs = new \Twig_Loader_Filesystem( $basedir );
-        $twig 	 = new \Twig_Environment( $twig_fs, $twig_args );
-        $twig->addExtension( new \Clarkson_Core_Twig_Extension()    );
-        $twig->addExtension( new \Twig_Extensions_Extension_I18n()  );
-        $twig->addExtension( new \Twig_Extensions_Extension_Text()  );
-        $twig->addExtension( new \Twig_Extensions_Extension_Array() );
-        $twig->addExtension( new \Twig_Extensions_Extension_Date()  );
-
-        $filelist = $this->getFileList( $basedir );
-
-        foreach($filelist as $filepath) {
-            $truepath = str_replace( $basedir, '', $filepath );
-            $twig->loadTemplate( $truepath );
-        }
-    }
-
-    protected function getFileList( $dir ) {
-        $filelist = array();
-        $directories = glob( realpath( $dir ). '/*', GLOB_ONLYDIR );
-        foreach( $directories as $directory ){
-            $filelist = array_merge( $filelist, $this->getFileList( $directory ) );
-        }
-        $filelist = array_merge( $filelist, glob( realpath( $dir ) . '/*.twig' ) );
-        return $filelist;
-    }
+$autoload = __DIR__ . '/../../../vendor/autoload.php';
+if ( file_exists( $autoload ) ) {
+	require_once $autoload;
 }
 
-\WP_CLI::add_command('clarkson-twig-translations', "\Clarkson\WPCLITwigTranslations\Gettext");
+if( ! class_exists( '\Clarkson\WPCLITwigTranslations\Gettext' ) ){
+    class Gettext extends \WP_CLI_Command {
+        public function __invoke( $args, $assoc_args ) {
+            $twig_args = array(
+                'debug' => true,
+                'cache' => __DIR__ . '/../../../' . $assoc_args['output-dir'] ,
+                'auto_reload' => true
+            );
+
+            $basedir = realpath( __DIR__ . '/../../../' . $assoc_args['template-dir'] );
+            $twig_fs = new \Twig_Loader_Filesystem( $basedir );
+            $twig 	 = new \Twig_Environment( $twig_fs, $twig_args );
+            $twig->registerUndefinedFunctionCallback(function ($name) {
+                return new \Twig\TwigFunction($name, $name);
+            });
+            $twig->addExtension( new \Twig_Extensions_Extension_I18n() );
+            $twig->addExtension( new \Twig_Extensions_Extension_Text() );
+            $twig->addExtension( new \Twig_Extensions_Extension_Array() );
+            $twig->addExtension( new \Twig_Extensions_Extension_Date() );
+
+            $filelist = $this->getFileList( $basedir );
+
+            foreach($filelist as $filepath) {
+                $truepath = str_replace( $basedir, '', $filepath );
+                $twig->loadTemplate( $truepath );
+            }
+        }
+
+        protected function getFileList( $dir ) {
+            $filelist = array();
+            $directories = glob( realpath( $dir ). '/*', GLOB_ONLYDIR );
+            foreach( $directories as $directory ){
+                $filelist = array_merge( $filelist, $this->getFileList( $directory ) );
+            }
+            $filelist = array_merge( $filelist, glob( realpath( $dir ) . '/*.twig' ) );
+            return $filelist;
+        }
+    }
+
+    \WP_CLI::add_command('clarkson-twig-translations', "\Clarkson\WPCLITwigTranslations\Gettext", array(
+        'synopsis' => array(
+            array(
+                'type'        => 'assoc',
+                'name'        => 'template-dir',
+                'description' => 'Directory where twig templates are found.',
+                'optional'    => true,
+                'default'     => '/templates/',
+            ),
+            array(
+                'type'        => 'assoc',
+                'name'        => 'output-dir',
+                'description' => 'Directory where rendered PHP files are saved.',
+                'optional'    => true,
+                'default'     => '/dist/rendered-templates/',
+            ),
+        ),
+        'when' => 'before_wp_load',
+    ));
+}
