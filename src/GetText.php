a3fa573c7e38dc427720d5252b16e7447ca78af5
Niels de Blaauw <niels@level-level.com>
2023-11-14 17:04:24 +0100

    Fix loading and directory structure.

--- src/GetText.php
+++ src/GetText.php
@@ -6,17 +6,18 @@ use Twig\Extra\Html\HtmlExtension;
 use Twig\Extra\Intl\IntlExtension;
 use Twig\Extra\Markdown\MarkdownExtension;
 use Twig\Extra\String\StringExtension;
+use WP_CLI;
 use WP_CLI_Command;
 
 class GetText extends WP_CLI_Command {
     public function __invoke( $args, $assoc_args ) {
         $twig_args = array(
             'debug' => true,
-            'cache' => __DIR__ . '/../../../' . $assoc_args['output-dir'] ,
+            'cache' => __DIR__ . '/../../../../' . $assoc_args['output-dir'] ,
             'auto_reload' => true
         );
 
-        $basedir = realpath( __DIR__ . '/../../../' . $assoc_args['template-dir'] );
+        $basedir = realpath( __DIR__ . '/../../../../' . $assoc_args['template-dir'] );
         $twig_fs = new \Twig\Loader\FilesystemLoader( $basedir );
         $twig 	 = new \Twig\Environment( $twig_fs, $twig_args );
         $twig->registerUndefinedFunctionCallback(function ($name) {
@@ -34,6 +35,8 @@ class GetText extends WP_CLI_Command {
             $truepath = str_replace( $basedir, '', $filepath );
             $twig->load( $truepath );
         }
+
+        WP_CLI::success( 'Twig templates loaded' );
     }
 
     protected function getFileList( $dir ) {
